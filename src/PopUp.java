b0708354499514f104dfae8e697a8025b7951776
rjcloughl <rjcloughley2001@gmail.com>
2022-04-15 13:05:27 -0400

    cleaned up popup and login screen in main

--- src/PopUp.java
+++ src/PopUp.java
@@ -5,23 +5,23 @@ import java.util.*;
 import java.io.*;
 import javax.swing.*;
 
-class PopUp{
-    public PopUp(){
+class PopUp {
+    public PopUp() {
         JFrame frame = new JFrame();
-        frame.setSize(250,150);
+        frame.setSize(250, 150);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JLabel label1 = new JLabel("Do you want to login now?");
         JButton button1 = new JButton("Yes");
         JButton button2 = new JButton("Exit");
         Container container = frame.getContentPane();
-        label1.setBounds(40,30,200,20);
-        button1.setBounds(40,60,60,20);
-        button2.setBounds(120,60,60,20);
+        label1.setBounds(40, 30, 200, 20);
+        button1.setBounds(40, 60, 60, 20);
+        button2.setBounds(120, 60, 60, 20);
 
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == button1){
+                if (e.getSource() == button1) {
                     new Login();
                     frame.dispose();
                 }
@@ -30,7 +30,7 @@ class PopUp{
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (e.getSource()==button2){
+                if (e.getSource() == button2) {
                     frame.dispose();
                 }
             }
@@ -42,9 +42,5 @@ class PopUp{
         container.setLayout(null);
         frame.setVisible(true);
 
-    }
-
-    public static void main(String[] args) {
-        PopUp o = new PopUp();
     }
 }
